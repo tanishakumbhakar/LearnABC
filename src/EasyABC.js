23a34189c6b6323182cb22ab72518a64ad3e34a1
TANISHA KUMBHAKAR <46387444+tanishakumbhakar@users.noreply.github.com>
2020-05-27 10:10:45 +0530

    Making content load from alphabets.json

--- src/EasyABC.js
+++ src/EasyABC.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
+import alphabets from './alphabets.json';
 class EasyABC extends Component{
+
+	constructor(props){
+		super(props);
+		this.state={
+			alphabets:alphabets,
+			currentPosition:0
+		};
+	}
 	render(){
+			console.log(alphabets);
 		return(
+
 			<div className="game">
 				<div className="option">
 					<div className="fields">
-						<div className="field-block">A</div>
+						<div className="field-block">{this.state.alphabets[this.state.currentPosition].letter}</div>
 					</div>
 					<div className="buttons">
-						<a href="#" className="button prev">Previous</a>
-						<a href="#" className="button sound">Play Sound Again</a>
-						<a href="#" className="button next">Next</a>
+						<a  className="button prev">Previous</a>
+						<a  className="button sound">Play Sound Again</a>
+						<a  className="button next">Next</a>
 					</div>
 					<div className="fields">
 						<div className="field-block">
-							<div className="left-field">A</div>
-							<div className="right-field">A</div>
+							<div className="left-field">
+								<div className="placeholder-span hide">Click Next to view Image</div>
+								<img className="letter-image" src={this.state.alphabets[this.state.currentPosition].image} alt={this.state.alphabets[this.state.currentPosition].word}/>
+							</div>
+							<div className="right-field">
+
+								<div className="placeholder-span hide">Click Next to view Spelling</div>
+								<div className="word">{this.state.alphabets[this.state.currentPosition].word.toUpperCase()}</div>
+							</div>
 						</div>
 					</div>
 				</div>
